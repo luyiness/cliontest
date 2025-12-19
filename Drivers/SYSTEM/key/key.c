2a9e605378589b70ca63250b3fc2c89886aea3f9
luyi2 <luyiness@gmail.com>
2025-12-19 12:39:24 +0800

    #fix key bug

--- Drivers/SYSTEM/key/key.c
+++ Drivers/SYSTEM/key/key.c
@@ -8,15 +8,15 @@
 void key_init(void) {
     __HAL_RCC_GPIOE_CLK_ENABLE();
 
-    GPIO_InitTypeDef GPIO_InitStruct = {GPIO_PIN_2, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_SPEED_FREQ_LOW};
+    GPIO_InitTypeDef GPIO_InitStruct = {GPIO_PIN_4, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_SPEED_FREQ_LOW};
     HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 }
 
 uint8_t key_scan(void) {
-    if (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_2)==0) {   //按键按下了
+    if (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_4)==0) {   //按键按下了
         delay_ms(10);   //消抖
-        if (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_2)==0) {   //按键还在按着
-            while (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_2)==0) {}   //等待按键松开
+        if (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_4)==0) {   //按键还在按着
+            while (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_4)==0) {}   //等待按键松开
             delay_ms(10);
             return 1;
         }
