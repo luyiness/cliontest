e3fbd76164d93c7ca0526ed222617bbc58ef8a47
luyi2 <luyiness@gmail.com>
2025-12-25 17:42:18 +0800

    atim repetition counter #fix bug

--- Drivers/SYSTEM/tim/atim_rc.c
+++ Drivers/SYSTEM/tim/atim_rc.c
@@ -14,7 +14,7 @@ void atim_rc_init(uint32_t prescaler,uint32_t rlr) {
     g_timx_rc_chy_handle.Init.RepetitionCounter = 0;
     HAL_TIM_PWM_Init(&g_timx_rc_chy_handle);
 
-    TIM_OC_InitTypeDef timx_oc_pwm_chy;
+    TIM_OC_InitTypeDef timx_oc_pwm_chy = {0};
     timx_oc_pwm_chy.OCMode = TIM_OCMODE_PWM1;
     timx_oc_pwm_chy.Pulse = rlr / 2;      //CCRx 比较值, 占空比50%
     timx_oc_pwm_chy.OCPolarity = TIM_OCPOLARITY_HIGH;
@@ -47,8 +47,8 @@ void TIM8_UP_IRQHandler(void)
 uint32_t g_pwm_count = 0 ;              /* CNT溢出次数 */
 void atim_timx_npwm_chy_set(uint8_t pwm_count) {
     g_pwm_count = pwm_count;    //注：pwm_count必须要>0
-    __HAL_TIM_ENABLE(&g_timx_rc_chy_handle);    //启动counter
     HAL_TIM_GenerateEvent(&g_timx_rc_chy_handle, TIM_EVENTSOURCE_UPDATE);   //启动后马上手动产生一次软件update interrupt，进入下面callback
+    __HAL_TIM_ENABLE(&g_timx_rc_chy_handle);    //启动counter
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {  //有update interrupt、进入此函数
@@ -56,6 +56,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {  //有update inter
         if (g_pwm_count) {
             TIM8->RCR = g_pwm_count - 1;
             HAL_TIM_GenerateEvent(&g_timx_rc_chy_handle, TIM_EVENTSOURCE_UPDATE);   //产生一次软件update interrupt，使RCR值到其影子
+            // __HAL_TIM_ENABLE(&g_timx_rc_chy_handle);    //启动counter
             g_pwm_count = 0;
         } else {
             TIM8->CR1 &= ~(1 << 0); //CR1的位0置0，就是关闭counter；（为什么不用HAL库的__HAL_TIM_DISABLE()：它执行关闭有前提条件）
